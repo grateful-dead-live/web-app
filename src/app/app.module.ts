2fc1f57bb73668ecb6b0611845206e90b5224e54
Florian Thalmann <florianthalmann@bluewin.ch>
2017-10-07 15:10:01 +0100

    added maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MdGridListModule, MdCardModule, MdToolbarModule, MdMenuModule, MdButtonModule, MdTooltipModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { DateAxis } from './date-axis.component';
@@ -20,9 +21,12 @@ import { DeadApiService } from './dead-api.service';
     MdToolbarModule,
     MdMenuModule,
     MdButtonModule,
+    MdTooltipModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MdTooltipModule
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBhBOyzthOjvWKAhax_o1clQYSdoSQIkd4'
+    })
   ],
   providers: [DeadApiService],
   bootstrap: [AppComponent]
